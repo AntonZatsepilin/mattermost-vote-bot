e3fa7fc152d7fdc956920aae82ef5e098c53661b
AntonZatsepilin <zatsepilin90.00@bk.ru>
2025-03-28 23:08:45 +0300

    Initialize Tarantool database connection and load environment variables

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,4 +1,53 @@
 package main
 
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/AntonZatsepilin/mattermost-vote-bot.git/internal/repository"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/tarantool/go-tarantool/v2"
+)
+
 func main() {
+logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := godotenv.Load(".env"); err != nil {
+		logrus.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error initializing config: %s", err.Error())
+	}
+
+	tarantoolCfg := repository.TarantoolConfig{
+		Host:     os.Getenv("TARANTOOL_HOST"),
+		Port:     os.Getenv("TARANTOOL_PORT"),
+		User:     os.Getenv("TARANTOOL_USER_NAME"),
+		Password: os.Getenv("TARANTOOL_USER_PASSWORD"),
+		Timeout: viper.GetInt("tarantool.timeout"),
+	}
+
+	logrus.Infof("Tarantool config: %v", tarantoolCfg)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+	defer cancel()
+
+	var db *tarantool.Connection
+	var err error
+	db, err = repository.NewTarantoolDB(ctx, tarantoolCfg)
+	if err != nil {
+		logrus.Fatalf("failed to initialize Tarantool: %s", err.Error())
+	}
+
+	defer db.CloseGraceful()
+}
+
+func initConfig() error {
+	viper.AddConfigPath("./configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- backend/go.mod
+++ backend/go.mod
@@ -3,14 +3,31 @@ module github.com/AntonZatsepilin/mattermost-vote-bot.git
 go 1.23.1
 
 require (
-	github.com/google/uuid v1.3.0
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/viper v1.20.1
 	github.com/tarantool/go-tarantool/v2 v2.3.0
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/sagikazarmark/locafero v0.7.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.12.0 // indirect
+	github.com/spf13/cast v1.7.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/tarantool/go-iproto v1.1.0 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )

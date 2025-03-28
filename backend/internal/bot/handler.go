a149009dd0bb0d4be3e968254485f916bcc1f9f1
AntonZatsepilin <zatsepilin90.00@bk.ru>
2025-03-28 23:34:47 +0300

    Add Mattermost and PostgreSQL services to Docker Compose configuration

--- backend/internal/bot/handler.go
+++ backend/internal/bot/handler.go
@@ -1,26 +1,32 @@
 package bot
 
-func (b *MattermostBot) handleMessage(postID string) {
-    post, resp := b.Client.GetPost(postID, "")
-    if resp.Error != nil {
-        b.Logger.Error(resp.Error)
-        return
-    }
-    
-    if post.UserId == b.User.Id {
-        return
-    }
-    
-    switch {
-    case strings.HasPrefix(post.Message, "/poll create"):
-        b.handleCreatePoll(post)
-    case strings.HasPrefix(post.Message, "/poll vote"):
-        b.handleVote(post)
-    case strings.HasPrefix(post.Message, "/poll results"):
-        b.handleResults(post)
-    }
-}
+// import (
+// 	"strings"
 
-func (b *MattermostBot) handleCreatePoll(post *model.Post) {
-    poll, err := b.Service.CreatePoll(...)
-}
+// 	"github.com/mattermost/mattermost-server/v5/model"
+// )
+
+// func (b *MattermostBot) handleMessage(postID string) {
+//     post, resp := b.Client.GetPost(postID, "")
+//     if resp.Error != nil {
+//         b.Logger.Error(resp.Error)
+//         return
+//     }
+    
+//     if post.UserId == b.User.Id {
+//         return
+//     }
+    
+//     switch {
+//     case strings.HasPrefix(post.Message, "/poll create"):
+//         b.handleCreatePoll(post)
+//     case strings.HasPrefix(post.Message, "/poll vote"):
+//         b.handleVote(post)
+//     case strings.HasPrefix(post.Message, "/poll results"):
+//         b.handleResults(post)
+//     }
+// }
+
+// func (b *MattermostBot) handleCreatePoll(post *model.Post) {
+//     poll, err := b.Service.CreatePoll(post.UserId, post.Message)
+// }
--- backend/internal/bot/mattermost.go
+++ backend/internal/bot/mattermost.go
@@ -1,53 +1,53 @@
 package bot
 
-import (
-	"github.com/AntonZatsepilin/mattermost-vote-bot.git/internal/service"
-	"github.com/mattermost/mattermost-server/v5/model"
-	"github.com/sirupsen/logrus"
-)
+// import (
+// 	"github.com/AntonZatsepilin/mattermost-vote-bot.git/internal/service"
+// 	"github.com/mattermost/mattermost-server/v5/model"
+// 	"github.com/sirupsen/logrus"
+// )
 
-type MattermostBot struct {
-    Client *model.Client4
-    WebSocket *model.WebSocketClient
-    User    *model.User
-    Service *service.Service
-    Logger  *logrus.Logger
-}
+// type MattermostBot struct {
+//     Client *model.Client4
+//     WebSocket *model.WebSocketClient
+//     User    *model.User
+//     Service *service.Service
+//     Logger  *logrus.Logger
+// }
 
-func NewBot(serverURL, token string) (*MattermostBot, error) {
-    client := model.NewAPIv4Client(serverURL)
-    client.SetToken(token)
-    
-    user, resp := client.GetMe("")
-    if resp.Error != nil {
-        return nil, resp.Error
-    }
-    
-    ws, err := model.NewWebSocketClient4(serverURL, token)
-    if err != nil {
-        return nil, err
-    }
-    
-    return &MattermostBot{
-        Client: client,
-        WebSocket: ws,
-        User:    user,
-        Logger:  logrus.New(),
-    }, nil
-}
+// func NewBot(serverURL, token string) (*MattermostBot, error) {
+//     client := model.NewAPIv4Client(serverURL)
+//     client.SetToken(token)
 
-func (b *MattermostBot) Start() {
-    b.WebSocket.Listen()
-    for {
-        select {
-        case event := <-b.WebSocket.EventChannel:
-            b.handleEvent(event)
-        }
-    }
-}
+//     user, resp := client.GetMe("")
+//     if resp.Error != nil {
+//         return nil, resp.Error
+//     }
 
-func (b *MattermostBot) handleEvent(event *model.WebSocketEvent) {
-    if event.EventType() == model.WEBSOCKET_EVENT_POSTED {
-        b.handleMessage(event.GetData()["post"].(string))
-    }
-}
+//     ws, err := model.NewWebSocketClient4(serverURL, token)
+//     if err != nil {
+//         return nil, err
+//     }
+
+//     return &MattermostBot{
+//         Client: client,
+//         WebSocket: ws,
+//         User:    user,
+//         Logger:  logrus.New(),
+//     }, nil
+// }
+
+// func (b *MattermostBot) Start() {
+//     b.WebSocket.Listen()
+//     for {
+//         select {
+//         case event := <-b.WebSocket.EventChannel:
+//             b.handleEvent(event)
+//         }
+//     }
+// }
+
+// func (b *MattermostBot) handleEvent(event *model.WebSocketEvent) {
+//     if event.EventType() == model.WEBSOCKET_EVENT_POSTED {
+//         b.handleMessage(event.GetData()["post"].(string))
+//     }
+// }
